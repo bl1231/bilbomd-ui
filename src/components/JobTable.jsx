3f477701c00a16853ed7ec90ed0f098b0b16558e
Scott Classen <sclassen@lbl.gov>
2023-01-27 15:32:34 -0800

    again

---
+++ src/components/JobTable.jsx
@@ -0,0 +1,155 @@
+import { useEffect, useState, useCallback, useMemo } from 'react';
+import {
+  DataGrid,
+  GridCellParams,
+  GridColDef,
+  GridValueFormatterParams,
+  GridRowId,
+  GridActionsCellItem,
+  GridColumns
+} from '@mui/x-data-grid';
+import { format, parseISO } from 'date-fns';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import SecurityIcon from '@mui/icons-material/Security';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import clsx from 'clsx';
+import { Box } from '@mui/system';
+
+import { green, red, amber } from '@mui/material/colors';
+
+const emptyJob = [
+  {
+    title: '',
+    pdbs: [{ name: '' }],
+    constinp: '',
+    expdata: '',
+    num_conf: '',
+    rg_min: '',
+    rg_max: '',
+    uuid: '',
+    status: '',
+    message: '',
+    submitted: '',
+    completed: ''
+  }
+];
+
+//type Job = typeof emptyJob[];
+
+export default function JobTable() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/jobs')
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
+
+  const deleteJob = useCallback(
+    (uuid) => () => {
+      setTimeout(() => {
+        setJobs((prevJobs) => prevJobs.filter((job) => job.uuid !== uuid));
+      });
+    },
+    []
+  );
+
+  const columns = useMemo(
+    () => [
+      { field: 'uuid', type: 'string', headerName: 'UUID', width: 70 },
+      {
+        field: 'submitted',
+        type: 'dateTime',
+        width: 160,
+        valueFormatter: (params: GridValueFormatterParams<string>) => {
+          if (params?.value) {
+            return format(parseISO(params?.value), 'MM/dd/yyyy HH:mm:ss');
+          } else {
+            return '--/--/-- 00:00:00';
+          }
+        }
+      },
+      {
+        field: 'completed',
+        type: 'dateTime',
+        width: 160,
+        valueFormatter: (params: GridValueFormatterParams<string>) => {
+          if (params?.value) {
+            return format(parseISO(params?.value), 'MM/dd/yyyy HH:mm:ss');
+          } else {
+            return '--/--/-- 00:00:00';
+          }
+        }
+      },
+      { field: 'title', headerName: 'Title', width: 200 },
+      {
+        field: 'status',
+        headerName: 'Status',
+        width: 100,
+        cellClassName: (params: GridCellParams<string>) => {
+          if (params.value == null) {
+            return '';
+          }
+          return clsx('bilbomd', {
+            submitted: params.value === 'submitted',
+            running: params.value === 'running',
+            error: params.value === 'error',
+            completed: params.value === 'completed'
+          });
+        }
+      },
+      {
+        field: 'actions',
+        type: 'actions',
+        width: 80,
+        getActions: (params) => [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={deleteJob(params.uuid)}
+          />
+        ]
+      }
+    ],
+    [deleteJob]
+  );
+
+  return (
+    <Box
+      sx={{
+        height: 500,
+        width: '100%',
+        '& .bilbomd.completed': {
+          backgroundColor: green[400],
+          color: '#1a3e72',
+          fontWeight: '800'
+        },
+        '& .bilbomd.error': {
+          backgroundColor: red[600],
+          color: '#1a3e72',
+          fontWeight: '600'
+        },
+        '& .bilbomd.running': {
+          backgroundColor: amber[200],
+          color: '#1a3e72',
+          fontWeight: '600'
+        },
+        '& .bilbomd.submitted': {
+          backgroundColor: amber[100],
+          color: '#1a3e72',
+          fontWeight: '600'
+        }
+      }}
+    >
+      <DataGrid
+        rows={jobs}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        getRowId={(row) => row.uuid}
+      />
+    </Box>
+  );
+}
