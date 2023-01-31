2fc1eb3bb19352a3e205db42a6db5a04870d1af2
Scott Classen <sclassen@lbl.gov>
2023-01-31 08:10:12 -0800

    remove unused code

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -3,35 +3,14 @@
 //import useLogout from 'hooks/useLogout';
 //import { useEffect, useState } from 'react';
 import { Container } from '@mui/system';
+import Typography from 'themes/overrides/Typography';
 import JobTable from './JobTable';
 //import { Typography } from '@mui/material';
 
 const Dashboard = () => {
-  //const { setAuth } = useAuth();
-  //const navigate = useNavigate();
-  //const logout = useLogout();
-  // const signOut = async () => {
-  //   await logout();
-  //   navigate('/linkpage');
-  // };
-  //const [jobs, setJobs] = useState([]);
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3333/jobs')
-  //     .then((res) => res.json())
-  //     .then((data) => setJobs(data));
-  // }, []);
-
-  // const handleDelete = async (uuid) => {
-  //   await fetch('http://localhost:3333/jobs' + uuid, {
-  //     method: 'DELETE'
-  //   });
-  //   const newJobs = jobs.filter((job) => job.uuid !== uuid);
-  //   setJobs(newJobs);
-  // };
-
   return (
     <Container>
+      <Typography> Your Jobs</Typography>
       <JobTable />
     </Container>
   );
