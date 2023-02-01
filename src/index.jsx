dae237b5f1d9b9b3128da5455c78b9610c90f6a1
Scott Classen <sclassen@lbl.gov>
2023-02-01 09:10:10 -0800

    remove more unused components

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// for original Dave Gray jwt auth
-//import { AuthProvider } from 'context/AuthProvider';
 
 import { store } from './store';
 import { Provider } from 'react-redux';
