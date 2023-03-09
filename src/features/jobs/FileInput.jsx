55bc29aa72f1a94ce58e0768191a53f219e51a13
Scott Classen <sclassen@lbl.gov>
2023-03-09 10:55:47 -0800

    add new custom FileInput component

---
+++ src/features/jobs/FileInput.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Button, FormControl, FormHelperText, FormLabel, Input } from '@mui/material'
+
+const FileInput = (props) => {
+  const [fileName, setFileName] = useState('')
+
+  const handleFileChange = (event) => {
+    event.preventDefault()
+    let reader = new FileReader()
+    let file = event.target.files[0]
+    if (file) {
+      reader.onloadend = () => setFileName(file.name)
+      reader.readAsDataURL(file)
+      props.setFieldValue(props.name, file)
+    }
+  }
+
+  return (
+    <FormControl margin="normal">
+      <FormLabel sx={{ marginBottom: '0.6em' }}>
+        Upload your <b>{props.fileType}</b> file
+      </FormLabel>
+      <Input
+        style={{ display: 'none' }}
+        id={props.id}
+        name={props.name}
+        type="file"
+        accept={props.fileExt}
+        onChange={handleFileChange}
+        onBlur={props.onBlur}
+      />
+      <label htmlFor={props.id}>
+        <Button variant="contained" component="span" sx={{ mr: 2, width: '100px' }}>
+          {props.title}
+        </Button>
+      </label>
+      {fileName ? (
+        <FormHelperText sx={{ ml: 0, color: '#389e0d', fontSize: 16 }}>
+          {fileName}
+        </FormHelperText>
+      ) : null}
+      {props.errorMessage ? (
+        <FormHelperText sx={{ ml: 0, color: '#cf1322', fontSize: 16 }} error={true}>
+          {props.errorMessage}
+        </FormHelperText>
+      ) : null}
+    </FormControl>
+  )
+}
+
+export default FileInput
