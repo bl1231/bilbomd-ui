4fac6228ab504ebef703f7d2887de1f76103c265
Scott Classen <sclassen@lbl.gov>
2023-04-25 15:14:59 -0700

    add PM2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps: [
+    {
+      name: 'BilboMD',
+      script: 'serve',
+      env: {
+        PM2_SERVE_PATH: 'build',
+        PM2_SERVE_PORT: 3001,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      env_development: {
+        NODE_ENV: 'development'
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'webadmin',
+      host: 'hyperion',
+      ref: 'origin/main',
+      repo: 'git@github.com:bl1231/bilbomd-ui.git',
+      path: '/bilbomd',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+}
