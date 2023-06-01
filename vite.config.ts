d84127fb9664ce3e8d5939f1989ed539a718e3d3
Scott Classen <sclassen@lbl.gov>
2023-06-01 14:24:25 -0700

    listen on port 3003 for dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: { port: 3002 },
+  server: { port: 3003 },
   build: {
     rollupOptions: {
       output: {
