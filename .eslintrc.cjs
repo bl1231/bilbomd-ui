686d42a16f0ecf4527a79b0540605f4250722a98
Scott Classen <sclassen@lbl.gov>
2023-06-15 12:26:18 -0700

    various small tooling tweaks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,18 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime'
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['react', 'react-hooks'],
-  rules: {}
+  plugins: ['react', 'react-hooks', 'react-refresh'],
+  rules: { 'react-refresh/only-export-components': 'warn' }
 }
