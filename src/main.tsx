2fa5ab3231ddc0a20e75fff0ac62288467dd114d
Scott Classen <sclassen@lbl.gov>
2023-06-15 12:25:24 -0700

    rename index to main

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+// import React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from 'app/store'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ReduxProvider>
+  </React.StrictMode>
+)
