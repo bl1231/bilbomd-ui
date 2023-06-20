edbccf6117d9696f8815990425b9e7b325d2ca8a
Scott Classen <sclassen@lbl.gov>
2023-06-20 15:39:58 -0700

    rename axios.js to axios.ts

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-console.log('axios', import.meta.env.MODE)
+// console.log('axios', import.meta.env.MODE)
 const baseURL = import.meta.env.DEV
   ? `http://localhost:${import.meta.env.VITE_BILBOMD_BACKEND_PORT}`
   : 'https://bl1231.als.lbl.gov/bilbomd-dev-backend'
@@ -7,9 +7,3 @@ const baseURL = import.meta.env.DEV
 export default axios.create({
   baseURL: baseURL
 })
-
-// export const axiosPrivate = axios.create({
-//   baseURL: baseUrl(),
-//   headers: { 'Content-Type': 'application/json' },
-//   withCredentials: true
-// })
