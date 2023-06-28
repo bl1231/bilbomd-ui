44497d2a1f1699e4df288e792cf0dcb835c5eb1d
Scott Classen <sclassen@lbl.gov>
2023-06-27 17:59:45 -0700

    nice start to better User edit component

--- src/features/users/EditUserForm.tsx
+++ src/features/users/EditUserForm.tsx
@@ -1,8 +1,6 @@
-import { useEffect, useState, ChangeEvent } from 'react'
+import { useEffect, useState } from 'react'
 import {
   Button,
-  Card,
-  CardContent,
   Checkbox,
   Grid,
   TextField,
@@ -13,7 +11,7 @@ import {
   FormControl,
   FormGroup,
   Select,
-  SelectChangeEvent,
+  Typography,
   InputLabel,
   OutlinedInput,
   ListItemText
@@ -25,11 +23,39 @@ import DialogContentText from '@mui/material/DialogContentText'
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import { Field, Form, Formik } from 'formik'
-import { Debug } from 'components/Debug'
+// import { Debug } from 'components/Debug'
+import { format } from 'date-fns'
 import { User, useUpdateUserMutation, useDeleteUserMutation } from './usersApiSlice'
+import { useGetJobsQuery } from 'features/jobs/jobsApiSlice'
+import JobSummary from 'features/jobs/JobSummary'
 import { ROLES } from 'config/roles'
 import { useNavigate } from 'react-router-dom'
 import { editUserSchema } from 'schemas/ValidationSchemas'
+import { Box } from '@mui/system'
+import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles'
+import Chip from '@mui/material/Chip'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark' ? '#1A2027' : theme.palette.background.paper,
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'left',
+  color: theme.palette.text.primary
+}))
+
+const HeaderThingee = {
+  textTransform: 'uppercase',
+  fontSize: 12,
+  borderTopLeftRadius: 4,
+  borderTopRightRadius: 4,
+  fontWeight: 500,
+  padding: '0.5rem',
+  background: '#888',
+  color: '#fff',
+  letterSpacing: '1px'
+}
 
 interface EditUserFormProps {
   user: User
@@ -48,6 +74,16 @@ const MenuProps = {
 
 const EditUserForm = ({ user }: EditUserFormProps) => {
   const [open, setOpen] = useState(false)
+
+  const { data: jobs } = useGetJobsQuery('jobsList', {})
+
+  let filteredJobs
+  if (jobs) {
+    filteredJobs = jobs.filter((job) => job.user === user.id)
+    console.log('got jobs: ', filteredJobs)
+  } else {
+    console.log('no jobs')
+  }
 
   const handleClickOpen = () => {
     setOpen(true)
@@ -98,131 +134,164 @@ const EditUserForm = ({ user }: EditUserFormProps) => {
   }
 
   const content = (
-    <Card>
-      <CardContent>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={editUserSchema}
-          onSubmit={myOnSubmit}
-          enableReinitialize={true}
-        >
-          {({ values, isSubmitting, handleChange, handleBlur, setFieldValue }) => (
-            <Form>
-              <Grid container direction="column">
-                <Grid item sx={{ my: 2, width: '300px' }}>
-                  <Field
-                    name="username"
-                    id="username"
-                    label="Username"
-                    type="text"
-                    autoComplete="off"
-                    disabled={isSubmitting}
-                    component={TextField}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.username || ''}
-                  />
-                </Grid>
-                <Grid item sx={{ my: 2, width: '300px' }}>
-                  <Field
-                    name="email"
-                    id="email"
-                    label="Email"
-                    type="email"
-                    autoComplete="off"
-                    fullWidth
-                    disabled={isSubmitting}
-                    component={TextField}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email || ''}
-                  />
-                </Grid>
-
-                <Grid item sx={{ my: 2 }}>
-                  <FormLabel component="legend">Make User active or inactive</FormLabel>
-                  <FormGroup>
-                    <FormControlLabel
-                      control={<Checkbox checked={values.active} />}
-                      label="Active"
-                      name="active"
+    <>
+      <Box>
+        <Typography sx={HeaderThingee}>Edit User</Typography>
+        <Item>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={editUserSchema}
+            onSubmit={myOnSubmit}
+            enableReinitialize={true}
+          >
+            {({ values, isSubmitting, handleChange, handleBlur, setFieldValue }) => (
+              <Form>
+                <Grid container direction="column">
+                  <Grid item sx={{ my: 2, width: '300px' }}>
+                    <Field
+                      name="username"
+                      id="username"
+                      label="Username"
+                      type="text"
+                      autoComplete="off"
+                      disabled={isSubmitting}
+                      component={TextField}
                       onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.username || ''}
                     />
-                  </FormGroup>
-                </Grid>
-                <Grid item sx={{ mb: 2 }}>
-                  {/* https://github.com/jaredpalmer/formik/issues/2123 */}
+                  </Grid>
+                  <Grid item sx={{ my: 2, width: '300px' }}>
+                    <Field
+                      name="email"
+                      id="email"
+                      label="Email"
+                      type="email"
+                      autoComplete="off"
+                      fullWidth
+                      disabled={isSubmitting}
+                      component={TextField}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email || ''}
+                    />
+                  </Grid>
 
-                  <FormLabel component="legend">Assign Roles</FormLabel>
-                  <FormControl sx={{ my: 2, width: 300 }}>
-                    <InputLabel id="roles">Roles</InputLabel>
-                    <Select
-                      labelId="roles"
-                      id="roles"
-                      multiple={true}
-                      value={values.roles}
-                      onChange={(e) => setFieldValue('roles', e.target.value as string)}
-                      input={<OutlinedInput label="Roles" />}
-                      renderValue={(selected) => selected.join(', ')}
-                      MenuProps={MenuProps}
+                  <Grid item sx={{ my: 2 }}>
+                    <FormLabel component="legend">Make User active or inactive</FormLabel>
+                    <FormGroup>
+                      <FormControlLabel
+                        control={<Checkbox checked={values.active} />}
+                        label="Active"
+                        name="active"
+                        onChange={handleChange}
+                      />
+                    </FormGroup>
+                  </Grid>
+                  <Grid item sx={{ mb: 2 }}>
+                    {/* https://github.com/jaredpalmer/formik/issues/2123 */}
+
+                    <FormLabel component="legend">Assign Roles</FormLabel>
+                    <FormControl sx={{ my: 2, width: 300 }}>
+                      <InputLabel id="roles">Roles</InputLabel>
+                      <Select
+                        labelId="roles"
+                        id="roles"
+                        multiple={true}
+                        value={values.roles}
+                        onChange={(e) => setFieldValue('roles', e.target.value as string)}
+                        input={<OutlinedInput label="Roles" />}
+                        renderValue={(selected) => selected.join(', ')}
+                        MenuProps={MenuProps}
+                      >
+                        {Object.values(ROLES).map((role) => (
+                          <MenuItem key={role} value={role}>
+                            <Checkbox checked={values.roles.indexOf(role) > -1} />
+                            <ListItemText primary={role} />
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      variant="contained"
+                      startIcon={<EditIcon />}
+                      sx={{ mr: 2 }}
+                      type="submit"
                     >
-                      {Object.values(ROLES).map((role) => (
-                        <MenuItem key={role} value={role}>
-                          <Checkbox checked={values.roles.indexOf(role) > -1} />
-                          <ListItemText primary={role} />
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
+                      Update
+                    </Button>
+                    <Button
+                      variant="contained"
+                      startIcon={<DeleteIcon />}
+                      sx={{ mr: 2 }}
+                      color="error"
+                      type="button"
+                      disabled={isSubmitting}
+                      onClick={handleClickOpen}
+                    >
+                      {`Delete ${values.username}`}
+                    </Button>
+                    <Dialog open={open} onClose={handleClose}>
+                      <DialogContent>
+                        <DialogContentText>
+                          {`Delete ${values.username} ?`}
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleDeleteUser} autoFocus>
+                          Delete
+                        </Button>
+                      </DialogActions>
+                    </Dialog>
+                  </Grid>
+                  <Grid item sx={{ mt: 2 }}>
+                    {updateResult.error || deleteResult.error ? (
+                      <Alert severity="warning">{errContent}</Alert>
+                    ) : (
+                      ''
+                    )}
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Button
-                    variant="contained"
-                    startIcon={<EditIcon />}
-                    sx={{ mr: 2 }}
-                    type="submit"
-                  >
-                    Update
-                  </Button>
-                  <Button
-                    variant="contained"
-                    startIcon={<DeleteIcon />}
-                    sx={{ mr: 2 }}
-                    color="error"
-                    type="button"
-                    disabled={isSubmitting}
-                    onClick={handleClickOpen}
-                  >
-                    {`Delete ${values.username}`}
-                  </Button>
-                  <Dialog open={open} onClose={handleClose}>
-                    <DialogContent>
-                      <DialogContentText>
-                        {`Delete ${values.username} ?`}
-                      </DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                      <Button onClick={handleClose}>Cancel</Button>
-                      <Button onClick={handleDeleteUser} autoFocus>
-                        Delete
-                      </Button>
-                    </DialogActions>
-                  </Dialog>
-                </Grid>
-                <Grid item sx={{ mt: 2 }}>
-                  {updateResult.error || deleteResult.error ? (
-                    <Alert severity="warning">{errContent}</Alert>
-                  ) : (
-                    ''
-                  )}
-                </Grid>
-              </Grid>
-              {process.env.NODE_ENV === 'development' ? <Debug /> : ''}
-            </Form>
-          )}
-        </Formik>
-      </CardContent>
-    </Card>
+              </Form>
+            )}
+          </Formik>
+        </Item>
+      </Box>
+      <Box sx={{ my: 1 }}>
+        <Typography sx={HeaderThingee}>User Details</Typography>
+        <Item>
+          <Typography>
+            <b>Created:</b> {format(new Date(user.createdAt), 'MM/dd/yyyy HH:mm:ss')}
+          </Typography>
+          <Typography>
+            <b>Last Modified:</b>{' '}
+            {format(new Date(user.updatedAt), 'MM/dd/yyyy HH:mm:ss')}
+          </Typography>
+          <Typography>
+            <b>UUID:</b> {user.UUID}
+          </Typography>
+        </Item>
+      </Box>
+      <Box sx={{ my: 1 }}>
+        <Typography sx={HeaderThingee}>
+          <Chip label={`Jobs ${filteredJobs.length}`} color="success" />
+        </Typography>
+        {filteredJobs.length >= 1 ? (
+          <Item>
+            {' '}
+            {filteredJobs.map((job, index) => (
+              <JobSummary key={index} job={job} />
+            ))}
+          </Item>
+        ) : (
+          <Item>no jobs for this user</Item>
+        )}
+      </Box>
+      {/* {process.env.NODE_ENV === 'development' ? <Debug /> : ''} */}
+    </>
   )
   return content
 }
