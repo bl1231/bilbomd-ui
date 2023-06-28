a071179f93b5e076768ff564e2a53ff88a29e30e
Scott Classen <dsclassen@users.noreply.github.com>
2023-06-28 09:05:24 -0700

    131 add details to user page (#159)
    * added a new JobSummary component
    
    * use Link instead of useNavigate
    
    * small format fix for Edit column
    
    * nice start to better User edit component
    
    * move `Item` outside of ternary

--- src/features/jobs/JobDetails.tsx
+++ src/features/jobs/JobDetails.tsx
@@ -1,8 +1,9 @@
 // import React from 'react'
 import InfoIcon from '@mui/icons-material/Info'
 import { IconButton, Tooltip } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+
 import { Job } from './jobsApiSlice'
+import { Link } from 'react-router-dom'
 
 interface JobDetailsProps {
   job: Job
@@ -10,16 +11,11 @@ interface JobDetailsProps {
 
 const JobDetails = ({ job }: JobDetailsProps) => {
   const { id, title } = job
-  const navigate = useNavigate()
+
   return (
     <>
       <Tooltip title={`Details for ${title}`} arrow>
-        <IconButton
-          color="secondary"
-          onClick={() => {
-            navigate(id)
-          }}
-        >
+        <IconButton color="secondary" component={Link} to={`/dashboard/jobs/${id}`}>
           <InfoIcon />
         </IconButton>
       </Tooltip>
---
+++ src/features/jobs/JobSummary.tsx
@@ -0,0 +1,37 @@
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import JobDetails from './JobDetails'
+import DeleteJob from './DeleteJob'
+import { Job } from './jobsApiSlice'
+
+interface JobSummaryProps {
+  job: Job
+}
+
+const JobSummary = ({ job }: JobSummaryProps) => {
+  return (
+    <>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Card variant="outlined" sx={{ marginBottom: '16px' }}>
+          <CardContent>
+            <Typography
+              variant="h5"
+              component="h3"
+              sx={{ display: 'flex', alignItems: 'center' }}
+            >
+              {job.title}
+              <JobDetails job={job} />
+              <DeleteJob job={job} />
+            </Typography>
+            <Typography variant="body2">Status: {job.status}</Typography>
+            <Typography variant="body2">Created At: {job.createdAt}</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </>
+  )
+}
+
+export default JobSummary
