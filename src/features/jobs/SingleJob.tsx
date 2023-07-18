cbda49e0abad94e0a78286f2fd1073d9cc5224cb
Scott Classen <sclassen@lbl.gov>
2023-07-18 08:32:18 -0700

    check this in until I can clean up jobs components

---
+++ src/features/jobs/SingleJob.tsx
@@ -0,0 +1,194 @@
+import { useParams } from 'react-router-dom'
+import { useGetJobsQuery } from './jobsApiSlice'
+import PulseLoader from 'react-spinners/PulseLoader'
+import useTitle from 'hooks/useTitle'
+import { Button, Divider, Grid, Typography } from '@mui/material'
+// import { Box } from '@mui/system'
+import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles'
+import axiosInstance, { AxiosResponse } from 'app/api/axios'
+import { format } from 'date-fns'
+import MissingJob from 'components/MissingJob'
+import { Job } from 'types/interfaces'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark' ? '#1A2027' : theme.palette.background.paper,
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'left',
+  color: theme.palette.text.primary
+}))
+
+const HeaderThingee = {
+  textTransform: 'uppercase',
+  fontSize: 12,
+  borderTopLeftRadius: 4,
+  borderTopRightRadius: 4,
+  fontWeight: 500,
+  padding: '0.5rem',
+  background: '#888',
+  color: '#fff',
+  letterSpacing: '1px'
+}
+
+const SingleJob = () => {
+  useTitle('BilboMD: Job Details')
+
+  const { id } = useParams()
+
+  // Will select the job with the given id, and will only rerender if the given jobs data changes
+  const { job } = useGetJobsQuery('jobsList', {
+    selectFromResult: ({ data }) =>
+      ({ job: data?.find((job) => job.id === id) } as { job: Job })
+  })
+  console.log(job)
+  if (!job) {
+    console.log('no job with id: ', id)
+    return <PulseLoader color={'#FFF'} />
+  }
+
+  const handleDownload = async (id: string) => {
+    try {
+      const response: AxiosResponse<Blob> = await axiosInstance.get(
+        `jobs/${id}/results`,
+        {
+          responseType: 'blob'
+        }
+      )
+
+      if (response.data) {
+        const url = window.URL.createObjectURL(response.data)
+        const link = document.createElement('a')
+        link.href = url
+        link.setAttribute('download', 'results.tar.gz')
+        document.body.appendChild(link)
+        link.click()
+        link.parentNode?.removeChild(link)
+      } else {
+        console.error('No data')
+      }
+    } catch (error) {
+      console.error('Download results.tar.gz error:', error)
+    }
+  }
+
+  const content = job ? (
+    <>
+      <Grid container spacing={2} rowSpacing={2}>
+        <Grid item xs={9}>
+          <Typography sx={HeaderThingee}>JOB TITLE</Typography>
+          <Item>
+            <Typography variant="h3" sx={{ ml: 1 }}>
+              {job.title}
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography sx={HeaderThingee}>STATUS</Typography>
+          <Item>
+            <Typography variant="h3" sx={{ ml: 1 }}>
+              {job.status}
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography sx={HeaderThingee}>DETAILS</Typography>
+          <Item>
+            <Grid container>
+              <Grid item xs={1}>
+                <Typography>
+                  <b>Time Submitted:</b>
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography>
+                  {format(new Date(job.time_submitted), 'MM/dd/yyyy HH:mm:ss')}
+                </Typography>
+              </Grid>
+              <Grid item xs={1}>
+                <Typography>
+                  <b>Time Started:</b>
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography>
+                  {job.time_started
+                    ? format(new Date(job.time_started), 'MM/dd/yyyy HH:mm:ss')
+                    : job.status}
+                </Typography>
+              </Grid>
+              <Grid item xs={1}>
+                <Typography>
+                  <b>Time Completed:</b>{' '}
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography>
+                  {job.time_completed
+                    ? format(new Date(job.time_completed), 'MM/dd/yyyy HH:mm:ss')
+                    : job.status}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Divider sx={{ my: 2 }} />
+
+            <Typography sx={{ ml: 1 }}>
+              <b>data:</b> {job.data_file}
+            </Typography>
+            <Typography sx={{ ml: 1 }}>
+              <b>psf_file:</b> {job.psf_file}
+            </Typography>
+            <Typography sx={{ ml: 1 }}>
+              <b>crd_file:</b> {job.crd_file}
+            </Typography>
+            <Typography sx={{ ml: 1 }}>
+              <b>const_inp_file:</b> {job.const_inp_file}
+            </Typography>
+            <Typography sx={{ ml: 1 }}>
+              <b>rg_min:</b> {job.rg_min}
+            </Typography>
+            <Typography sx={{ ml: 1 }}>
+              <b>rg_max:</b> {job.rg_max}
+            </Typography>
+
+            <Divider sx={{ my: 2 }} />
+
+            <Typography sx={{ ml: 1 }}>
+              <b>ID:</b> {job.id}
+            </Typography>
+            <Typography sx={{ ml: 1 }}>
+              <b>UUID:</b> {job.uuid}
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography sx={HeaderThingee}>RESULTS</Typography>
+          <Item>
+            {job.status === 'Completed' ? (
+              <Button
+                variant="contained"
+                onClick={() => {
+                  handleDownload(job.id)
+                }}
+                sx={{ my: 2 }}
+              >
+                Download Results
+              </Button>
+            ) : (
+              <Typography variant="h5" sx={{ m: 1 }}>
+                Pending...
+              </Typography>
+            )}
+          </Item>
+        </Grid>
+      </Grid>
+    </>
+  ) : (
+    <MissingJob id={id} />
+  )
+
+  return content
+}
+
+export default SingleJob
