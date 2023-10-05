cd91cd1e2e55dca8719ad77ca4948b4f9908dbab
Scott Classen <sclassen@lbl.gov>
2023-10-05 09:07:00 -0700

    add a standalone expdataSchema used for checking expdata before AutoRg

--- src/schemas/ValidationSchemas.ts
+++ src/schemas/ValidationSchemas.ts
@@ -107,18 +107,14 @@ export const bilbomdJobSchema = object().shape({
         return false
       }
     )
-    .test(
-      'check-for-spaces',
-      'Only accept file with no spaces in the name.',
-      async (file) => {
-        if (file) {
-          const spaceCheck = await noSpaces(file as File)
-          // console.log(spaceCheck)
-          return spaceCheck
-        }
-        return false
+    .test('check-for-spaces', 'No spaces allowed in the file name.', async (file) => {
+      if (file) {
+        const spaceCheck = await noSpaces(file as File)
+        // console.log(spaceCheck)
+        return spaceCheck
       }
-    ),
+      return false
+    }),
   crd_file: mixed()
     .test('required', 'CRD file obtained from CHARMM-GUI is required', (file) => {
       if (file) return true
@@ -188,18 +184,14 @@ export const bilbomdJobSchema = object().shape({
       }
       return false
     })
-    .test(
-      'check-for-spaces',
-      'Only accept file with no spaces in the name.',
-      async (file) => {
-        if (file) {
-          const spaceCheck = await noSpaces(file as File)
-          // console.log(spaceCheck)
-          return spaceCheck
-        }
-        return false
+    .test('check-for-spaces', 'No spaces allowed in the file name.', async (file) => {
+      if (file) {
+        const spaceCheck = await noSpaces(file as File)
+        // console.log(spaceCheck)
+        return spaceCheck
       }
-    ),
+      return false
+    }),
   expdata: mixed()
     .test('required', 'Experimental SAXS data is required', (file) => {
       if (file) return true
@@ -261,6 +253,47 @@ export const bilbomdJobSchema = object().shape({
     .max(100)
     .required('Please provide a Maximum Rg value')
 })
+export const expdataSchema = mixed()
+  .test('required', 'Experimental SAXS data is required', (file) => {
+    if (file) return true
+    return false
+  })
+  .test('file-size-check', 'Max file size is 2MB', (file) => {
+    if (file && (file as File).size <= 2000000) {
+      // console.log(file.size)
+      return true
+    }
+    // console.log(file.size)
+    return false
+  })
+  .test('file-type-check', 'Only accepts a *.dat file.', (file) => {
+    if (file && (file as File).name.split('.').pop()?.toUpperCase() === 'DAT') {
+      // console.log(file.name.split('.').pop())
+      return true
+    }
+    return false
+  })
+  .test('saxs-data-check', 'File does not appear to be SAXS data', async (file) => {
+    if (file) {
+      const saxsData = await isSaxsData(file as File)
+      // console.log('saxsData:', saxsData)
+      return saxsData
+    }
+    // additional return if test fails for reasons other than NOT being SAXS data
+    return false
+  })
+  .test(
+    'check-for-spaces',
+    'Only accept file with no spaces in the name.',
+    async (file) => {
+      if (file) {
+        const spaceCheck = await noSpaces(file as File)
+        // console.log(spaceCheck)
+        return spaceCheck
+      }
+      return false
+    }
+  )
 export const editUserSchema = object().shape({
   active: boolean()
 })
