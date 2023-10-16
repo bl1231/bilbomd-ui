7013327ae809d3f0b3e3df6befdf2b312213a129
Scott Classen <sclassen@lbl.gov>
2023-10-16 15:15:19 -0700

    try new labels for Jobs sidebar label

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -41,21 +41,21 @@ export default function ClippedDrawer() {
       roles: ['user', 'manager']
     },
     {
-      text: 'New Job',
+      text: 'BilboMD',
       icon: <AddCircleOutlineOutlined color="secondary" />,
       path: '/dashboard/jobs/new',
       onclick: () => navigate('dashboard/jobs/new'),
       roles: ['user', 'manager']
     },
     {
-      text: 'New Auto Job',
+      text: 'BilboMD Auto',
       icon: <AddCircleOutlineOutlined color="secondary" />,
       path: '/dashboard/jobs/simple',
       onclick: () => navigate('dashboard/jobs/simple'),
       roles: ['user', 'manager']
     },
     {
-      text: 'Inp Jiffy',
+      text: 'inp Jiffy',
       icon: <AutoAwesome color="secondary" />,
       path: '/dashboard/jobs/constinp',
       onclick: () => navigate('dashboard/jobs/constinp'),
