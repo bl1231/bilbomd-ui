f07a6a95adb9530d7b4c7092a08adad63a8ba683
Scott Classen <sclassen@lbl.gov>
2023-11-16 11:09:42 -0800

    add a rollup visualizer to help analyze the size of bundled JS code needed for app

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    visualizer({ gzipSize: true, brotliSize: true, template: 'sunburst' }),
+    tsconfigPaths()
+  ],
   server: { port: 3002 },
   build: {
     rollupOptions: {
