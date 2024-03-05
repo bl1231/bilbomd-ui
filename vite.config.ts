6ac92ad9f5ccd266c1df0e8ca68e05837b3ebd15
Scott Classen <sclassen@lbl.gov>
2024-03-05 09:58:57 -0800

    configure Vite to proxy /api requests

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -12,7 +11,16 @@ export default defineConfig({
     visualizer({ gzipSize: true, brotliSize: true, template: 'sunburst' }),
     tsconfigPaths()
   ],
-  server: { port: 3002 },
+    server: {
+    port: 3002,
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:3501',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   build: {
     rollupOptions: {
       output: {
