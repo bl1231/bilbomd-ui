23544257f0ffc1f9a9b0bec45fc0e374522ad635
Scott Classen <sclassen@lbl.gov>
2024-03-20 18:00:46 -0700

    comment out some debug console logs

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 import type { RootState } from '../store'
 
-const baseURL = "/api/v1"
+const baseURL = '/api/v1'
 
 const baseQuery = fetchBaseQuery({
   baseUrl: baseURL,
@@ -24,11 +24,11 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions)
 
   if (result?.error?.status === 403) {
-    console.log('sending refresh token')
+    // console.log('sending refresh token')
 
     // send refreshToken to get new accessToken
     const refreshResult = await baseQuery('/auth/refresh', api, extraOptions)
-    console.log('refreshResult:', refreshResult)
+    // console.log('refreshResult:', refreshResult)
     if (refreshResult?.data) {
       // store the new accessToken
       api.dispatch(setCredentials({ ...refreshResult.data }))
