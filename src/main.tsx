5c2392740f07de3c860f9b64e7b585679b76dc59
Scott Classen <sclassen@lbl.gov>
2024-03-23 18:38:11 -0700

    use recommended createBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,28 +4,38 @@ import './index.css'
 import App from './App'
 import { Provider as ReduxProvider } from 'react-redux'
 import { store } from 'app/store'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { ThemeContextProvider } from 'themes/ThemeContextProvider'
 import { StyledEngineProvider } from '@mui/system'
 import { ErrorBoundary } from 'react-error-boundary'
 import ErrorFallback from 'components/ErrorFallback'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+// Create a router with createBrowserRouter
+const router = createBrowserRouter([
+  {
+    path: '/*',
+    element: <App />
+    // You can add loader and children here as needed
+    // e.g., loader: appLoader,
+    // children: [{ path: 'team', element: <Team />, loader: teamLoader }],
+  }
+])
 
-root.render(
-  <React.StrictMode>
-    <ThemeContextProvider>
-      <StyledEngineProvider injectFirst>
-        <ReduxProvider store={store}>
-          <BrowserRouter>
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement)
+
+  root.render(
+    <React.StrictMode>
+      <ThemeContextProvider>
+        <StyledEngineProvider injectFirst>
+          <ReduxProvider store={store}>
             <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {}}>
-              <Routes>
-                <Route path='/*' element={<App />} />
-              </Routes>
+              <RouterProvider router={router} />
             </ErrorBoundary>
-          </BrowserRouter>
-        </ReduxProvider>
-      </StyledEngineProvider>
-    </ThemeContextProvider>
-  </React.StrictMode>
-)
+          </ReduxProvider>
+        </StyledEngineProvider>
+      </ThemeContextProvider>
+    </React.StrictMode>
+  )
+}
