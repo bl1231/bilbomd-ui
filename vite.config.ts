32a36fa7261038ae324edf8367bcbe17e79466b0
Scott Classen <sclassen@lbl.gov>
2024-03-29 11:46:22 -0700

    read git hash file

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { visualizer } from 'rollup-plugin-visualizer'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [
@@ -9,7 +10,24 @@ export default defineConfig({
       include: '**/*.tsx'
     }),
     visualizer({ gzipSize: true, brotliSize: true, template: 'sunburst' }),
-    tsconfigPaths()
+    tsconfigPaths(),
+    {
+      name: 'inject-git-hash',
+      config: () => {
+        let gitHash
+        try {
+          gitHash = fs.readFileSync('src/git-hash.txt', 'utf-8').trim()
+          console.log('Git hash read successfully:', gitHash)
+        } catch (error) {
+          console.error('Error reading git-hash.txt:', error)
+        }
+        return {
+          define: {
+            'import.meta.env.VITE_GIT_HASH': JSON.stringify(gitHash)
+          }
+        }
+      }
+    }
   ],
   server: {
     host: 'localhost',
