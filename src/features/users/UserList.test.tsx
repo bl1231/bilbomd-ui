60699f9e8a48cfffbad6165432beb6b709ee0f1a
shreyasprabhakar <shrprabh@ttu.edu>
2024-11-06 19:19:01 -0600

    Add unit tests for UserList.test.tsx

---
+++ src/features/users/UserList.test.tsx
@@ -0,0 +1,79 @@
+// UsersList.test.tsx
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import { MemoryRouter } from 'react-router-dom'
+import { useGetUsersQuery } from 'slices/usersApiSlice'
+import UsersList from './UsersList'
+
+// Mock the useGetUsersQuery hook
+vi.mock('slices/usersApiSlice', () => ({
+  useGetUsersQuery: vi.fn()
+}))
+
+describe('UsersList Component', () => {
+  it('should display loading indicator when data is loading', () => {
+    ;(useGetUsersQuery as jest.Mock).mockReturnValue({
+      data: null,
+      isLoading: true,
+      isSuccess: false,
+      isError: false,
+      error: null
+    })
+
+    render(
+      <MemoryRouter>
+        <UsersList />
+      </MemoryRouter>
+    )
+
+    expect(screen.getByRole('progressbar')).toBeInTheDocument()
+  })
+
+  it('should display an error message when there is an error', () => {
+    ;(useGetUsersQuery as jest.Mock).mockReturnValue({
+      data: null,
+      isLoading: false,
+      isSuccess: false,
+      isError: true,
+      error: new Error('Failed to fetch')
+    })
+
+    render(
+      <MemoryRouter>
+        <UsersList />
+      </MemoryRouter>
+    )
+
+    expect(
+      screen.getByText(/an error occurred while fetching users/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should display users data in a table when data is successfully fetched', () => {
+    const mockUsers = [
+      {
+        id: '1',
+        username: 'shreyastest',
+        email: 'john@example.com',
+        roles: ['user'],
+        active: true
+      }
+    ]
+
+    ;(useGetUsersQuery as jest.Mock).mockReturnValue({
+      data: mockUsers,
+      isLoading: false,
+      isSuccess: true,
+      isError: false,
+      error: null
+    })
+
+    render(
+      <MemoryRouter>
+        <UsersList />
+      </MemoryRouter>
+    )
+
+    expect(screen.getByText(/shreyastest/i)).toBeInTheDocument()
+  })
+})
