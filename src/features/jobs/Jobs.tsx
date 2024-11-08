8c54952d5794500c0c1b40956e56f1f152949d18
Scott Classen <sclassen@lbl.gov>
2024-11-08 14:41:55 -0800

    center the little progress bars vertically

--- src/features/jobs/Jobs.tsx
+++ src/features/jobs/Jobs.tsx
@@ -188,6 +188,8 @@ const Jobs = () => {
               sx={{
                 display: 'flex',
                 alignItems: 'center',
+                justifyContent: 'center',
+                height: '100%',
                 width: '100%'
               }}
             >
