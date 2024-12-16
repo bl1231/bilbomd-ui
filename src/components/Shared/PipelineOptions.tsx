a252f64cdb5ab912fdb683753c0e6a8d51b73c28
shreyasprabhakar <shrprabh@ttu.edu>
2024-12-16 16:33:01 -0600

    refactor: enhance layout and styling of PipelineOptions component for improved responsiveness

--- src/components/Shared/PipelineOptions.tsx
+++ src/components/Shared/PipelineOptions.tsx
@@ -26,8 +26,19 @@ interface PipelineOptionsProps {
 const PipelineOptions = ({ pipelines, isLightMode }: PipelineOptionsProps) => (
   <List>
     {pipelines.map((pipeline, index) => (
-      <Paper key={index} className='bilbomd-pipeline' sx={{ mb: 2 }}>
-        <ListItem>
+      <Paper
+        key={index}
+        className='bilbomd-pipeline'
+        sx={{ mb: 2, display: 'flex', alignItems: 'center' }}
+      >
+        <ListItem
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: 2
+          }}
+        >
           <ListItemAvatar>
             <Avatar sx={{ backgroundColor: green[700] }}>
               <RocketLaunchIcon />
@@ -36,14 +47,18 @@ const PipelineOptions = ({ pipelines, isLightMode }: PipelineOptionsProps) => (
           <ListItemText
             primary={pipeline.title}
             secondary={pipeline.description}
-            sx={{ width: '25%' }}
+            sx={{ flex: '1 1 auto', minWidth: '200px' }}
           />
           <img
             src={
               isLightMode ? pipeline.imagePath.light : pipeline.imagePath.dark
             }
             alt={`Overview of ${pipeline.title}`}
-            style={{ maxWidth: '92%', height: 'auto' }}
+            style={{
+              maxWidth: '65%',
+              maxHeight: '200px',
+              objectFit: 'contain'
+            }}
           />
         </ListItem>
       </Paper>
