f572adf211adbfd52597493299fc8c612e8f795a
Shreyas Prabhakar <148508046+shrprabh@users.noreply.github.com>
2025-01-07 04:07:47 +0530

    812 toggle pipeline png images when dark mode is selected (#813)
    * BilboMD Classic: Enhance NewJobForm with dark mode support and improve image handling
    
    * Add dark mode support to NewAutoJobForm and update image handling
    
    * Enhance NewAlphaFoldJobForm with dark mode support and update image handling
    
    * add dark mode SANS schematic
    
    * factor out pipelineschematic component
    
    ---------
    
    Co-authored-by: Scott Classen <sclassen@lbl.gov>

--- src/features/jobs/NewJobForm.tsx
+++ src/features/jobs/NewJobForm.tsx
@@ -10,7 +10,8 @@ import {
   Paper,
   FormGroup,
   FormControlLabel,
-  Divider
+  Divider,
+  LinearProgress
 } from '@mui/material'
 import Grid from '@mui/material/Grid2'
 import { Link as RouterLink } from 'react-router'
@@ -31,15 +32,53 @@ import SendIcon from '@mui/icons-material/Send'
 import { expdataSchema } from 'schemas/ExpdataSchema'
 import { BilboMDClassicJobSchema } from 'schemas/BilboMDClassicJobSchema'
 import useAuth from 'hooks/useAuth'
-import LinearProgress from '@mui/material/LinearProgress'
 import HeaderBox from 'components/HeaderBox'
 import NerscStatusChecker from 'features/nersc/NerscStatusChecker'
 import FileSelect from './FileSelect'
 import { Debug } from 'components/Debug'
 import NewJobFormInstructions from './NewJobFormInstructions'
 import { useGetConfigsQuery } from 'slices/configsApiSlice'
+import { useTheme } from '@mui/material/styles'
+
+const PipelineSchematic = ({
+  isDarkMode,
+  pipeline
+}: {
+  isDarkMode: boolean
+  pipeline: string
+}) => (
+  <Grid size={{ xs: 12 }}>
+    <HeaderBox>
+      <Typography>
+        BilboMD Classic Schematic -{' '}
+        {pipeline === 'pdb' ? 'PDB inputs' : 'CRD/PSF inputs'}
+      </Typography>
+    </HeaderBox>
+    <Paper sx={{ p: 2 }}>
+      <img
+        src={
+          pipeline === 'pdb'
+            ? isDarkMode
+              ? '/images/bilbomd-classic-pdb-schematic-dark.png'
+              : '/images/bilbomd-classic-pdb-schematic.png'
+            : isDarkMode
+              ? '/images/bilbomd-classic-crd-schematic-dark.png'
+              : '/images/bilbomd-classic-crd-schematic.png'
+        }
+        alt={
+          pipeline === 'pdb'
+            ? 'Overview of BilboMD PDB pipeline'
+            : 'Overview of BilboMD CRD/PSF pipeline'
+        }
+        style={{ maxWidth: '100%', height: 'auto' }}
+      />
+    </Paper>
+  </Grid>
+)
 
 const NewJobForm = () => {
+  const theme = useTheme()
+  const isDarkMode = theme.palette.mode === 'dark'
   const [addNewJob, { isSuccess }] = useAddNewJobMutation()
   const [calculateAutoRg, { isLoading }] = useCalculateAutoRgMutation()
   const { email } = useAuth()
@@ -100,7 +139,6 @@ const NewJobForm = () => {
 
     try {
       const newJob = await addNewJob(form).unwrap()
-      // setJobid(newJob.jobid)
       setStatus(newJob)
     } catch (error) {
       console.error('rejected', error)
@@ -145,7 +183,7 @@ const NewJobForm = () => {
       // Delay validation to ensure form state has been updated
       setTimeout(() => {
         validateForm()
-      }, 0) // You can adjust the timeout, but even a 0ms timeout can be enough to push to the next event loop
+      }, 0)
     }
 
   const isFormValid = (values: typeof initialValues) => {
@@ -169,29 +207,7 @@ const NewJobForm = () => {
         <NewJobFormInstructions />
       </Grid>
 
-      <Grid size={{ xs: 12 }}>
-        <HeaderBox>
-          <Typography>
-            BilboMD Classic Schematic -{' '}
-            {selectedMode === 'pdb' ? 'PDB inputs' : 'CRD/PSF inputs'}
-          </Typography>
-        </HeaderBox>
-        <Paper sx={{ p: 2 }}>
-          <img
-            src={
-              selectedMode === 'pdb'
-                ? '/images/bilbomd-classic-pdb-schematic.png'
-                : '/images/bilbomd-classic-crd-schematic.png'
-            }
-            alt={
-              selectedMode === 'pdb'
-                ? 'Overview of BilboMD PDB pipeline'
-                : 'Overview of BilboMD CRD/PSF pipeline'
-            }
-            style={{ maxWidth: '100%', height: 'auto' }}
-          />
-        </Paper>
-      </Grid>
+      <PipelineSchematic isDarkMode={isDarkMode} pipeline={selectedMode} />
 
       <Grid size={{ xs: 12 }}>
         <HeaderBox>
@@ -450,12 +466,14 @@ const NewJobForm = () => {
                           file.
                           {values.bilbomd_mode === 'pdb' ? (
                             <>
+                              {' '}
                               For example, Protein Chain ID <b>A</b> will be
                               converted to segid <b>PROA</b> and DNA Chain ID{' '}
-                              <b>G</b> will be converted to segid <b>DNAG</b>
+                              <b>G</b> will be converted to segid <b>DNAG</b>.
                             </>
                           ) : (
                             <>
+                              {' '}
                               Keeping in mind that CHARMM-GUI creates segid
                               names in a unique way.
                             </>
