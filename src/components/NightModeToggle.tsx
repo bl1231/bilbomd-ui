aacf5ce664c71c7ccc94979b1c40d2c0c9ac13c8
Scott Classen <sclassen@lbl.gov>
2025-01-10 15:50:47 -0800

    improve background for toggle button

--- src/components/NightModeToggle.tsx
+++ src/components/NightModeToggle.tsx
@@ -3,8 +3,6 @@ import Brightness4Icon from '@mui/icons-material/Brightness4'
 import Brightness7Icon from '@mui/icons-material/Brightness7'
 import { useThemeContext } from '../themes/ThemeContext'
 
-// interface NightModeToggleProps extends ButtonProps {}
-
 const NightModeToggle: React.FC<ButtonProps> = ({ sx, ...props }) => {
   const { mode, toggleColorMode } = useThemeContext()
 
@@ -14,7 +12,16 @@ const NightModeToggle: React.FC<ButtonProps> = ({ sx, ...props }) => {
       color='inherit'
       size='large'
       endIcon={mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      sx={sx}
+      sx={{
+        backgroundColor: mode === 'dark' ? 'grey.800' : 'grey.200',
+        color: mode === 'dark' ? 'grey.200' : 'grey.800',
+        '&:hover': {
+          backgroundColor: mode === 'dark' ? 'grey.700' : 'grey.300'
+        },
+        borderRadius: '20px', // Optional: adds a rounded look
+        px: 2, // Optional: padding for a better button size
+        ...sx
+      }}
       {...props}
     >
       {mode === 'dark' ? `light mode` : `dark mode`}
