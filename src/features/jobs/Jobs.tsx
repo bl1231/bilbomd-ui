24825e2240a0a5a3fe6a85e9b82fbaaf9c25290c
Scott Classen <sclassen@lbl.gov>
2025-05-02 11:03:02 -0700

    move styledmenu to themes components

--- src/features/jobs/Jobs.tsx
+++ src/features/jobs/Jobs.tsx
@@ -22,8 +22,7 @@ import {
   Select,
   SelectChangeEvent
 } from '@mui/material'
-import Menu, { MenuProps } from '@mui/material/Menu'
-import { styled, alpha } from '@mui/material/styles'
+import { StyledMenu } from 'themes/components/StyledDropdownMenu'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import { useDeleteJobMutation } from 'slices/jobsApiSlice'
 import {
@@ -88,49 +87,6 @@ const filteredJobCountChip = (count: number) => {
     />
   )
 }
-
-const StyledMenu = styled((props: MenuProps) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right'
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right'
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 140,
-    color:
-      theme.palette.mode === 'light'
-        ? 'rgb(55, 65, 81)'
-        : theme.palette.grey[300],
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0'
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5)
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity
-        )
-      }
-    }
-  }
-}))
 
 const jobTypeToRoute: Record<string, string> = {
   BilboMdPDB: 'classic',
@@ -554,8 +510,11 @@ const Jobs = () => {
                 }}
                 disableRipple
                 disabled={['Running', 'Submitted'].includes(params.row.status)}
+                sx={{
+                  color: 'error.main'
+                }}
               >
-                <DeleteIcon />
+                <DeleteIcon color='error' />
                 Delete
               </MenuItem>
             </StyledMenu>
